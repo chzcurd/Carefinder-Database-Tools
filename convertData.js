c3deb68149a170db07fb5d07a5cc6e85d088ade5
Josh <darkernight0@gmail.com>
2022-11-07 10:39:10 -0600

    simplify map function

--- convertData.js
+++ convertData.js
@@ -16,32 +16,26 @@ const theData = new XMLParser(options).parse(data);
 const inputDataArr = theData.response.row.row;
 
 //Map the hospital data that is parsed to the proper format
-const mappedDataArr = [];
-inputDataArr.map((x) => {
-  const mappedData = {
-    //data that can be nicely mapped
-    provider_id: x.provider_id,
-    hospital_name: x.hospital_name,
-    address: x.address,
-    city: x.city,
-    state: x.state,
-    zip_code: x.zip_code,
-    county_name: x.county_name,
-    hospital_type: x.hospital_type,
-    hospital_ownership: x.hospital_ownership,
-    emergency_services: x.emergency_services,
+const mappedDataArr = inputDataArr.map((x) => ({
+  //data that can be nicely mapped
+  provider_id: x.provider_id,
+  hospital_name: x.hospital_name,
+  address: x.address,
+  city: x.city,
+  state: x.state,
+  zip_code: x.zip_code,
+  county_name: x.county_name,
+  hospital_type: x.hospital_type,
+  hospital_ownership: x.hospital_ownership,
+  emergency_services: x.emergency_services,
 
-    //phone number is stored as { '@_phone_number': '9072624404' } for some reason
-    phone_number: x.phone_number["@_phone_number"],
+  //phone number is stored as { '@_phone_number': '9072624404' } for some reason
+  phone_number: x.phone_number["@_phone_number"],
 
-    //Pull location data from location object
-    latitude: x.location["@_latitude"],
-    longitude: x.location["@_longitude"],
-  };
-
-  //add mapped object to return array
-  mappedDataArr.push(mappedData);
-});
+  //Pull location data from location object
+  latitude: x.location["@_latitude"],
+  longitude: x.location["@_longitude"],
+}));
 
 //Stringify JSON and write to file
 const hospitalJSON = JSON.stringify(mappedDataArr);
